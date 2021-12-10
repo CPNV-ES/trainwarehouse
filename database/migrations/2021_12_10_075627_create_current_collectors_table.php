2b4e4c2ef913994137729e5c634f954b8f345795
AndiSO-1 <andi.santos-oliveira@cpnv.ch>
2021-12-10 09:01:27 +0100

    Add current collector migration

---
+++ database/migrations/2021_12_10_075627_create_current_collectors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrentCollectorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('current_collectors', function (Blueprint $table) {
+            $table->id();
+            $table->int('length_mm');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('current_collectors');
+    }
+}
